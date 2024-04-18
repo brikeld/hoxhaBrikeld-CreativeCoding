c6249c09b4dfa6aba08bd43275a10d227844e69c
brikeld <129516277+brikeld@users.noreply.github.com>
2024-04-18 23:20:20 +0200

    a
    a

--- 09-Sound/js/piano.js
+++ 09-Sound/js/piano.js
@@ -4,45 +4,46 @@ export default class Piano {
 constructor() {
   this.sampler =  new Tone.Sampler({
         urls: {
-          A0: "A0.mp3",
-          C1: "C1.mp3",
-          "D#1": "Ds1.mp3",
-          "F#1": "Fs1.mp3",
-          A1: "A1.mp3",
-          C2: "C2.mp3",
-          "D#2": "Ds2.mp3",
-          "F#2": "Fs2.mp3",
-          A2: "A2.mp3",
-          C3: "C3.mp3",
-          "D#3": "Ds3.mp3",
-          "F#3": "Fs3.mp3",
-          A3: "A3.mp3",
-          C4: "C4.mp3",
-          "D#4": "Ds4.mp3",
-          "F#4": "Fs4.mp3",
-          A4: "A4.mp3",
-          C5: "C5.mp3",
-          "D#5": "Ds5.mp3",
-          "F#5": "Fs5.mp3",
-          A5: "A5.mp3",
-          C6: "C6.mp3",
-          "D#6": "Ds6.mp3",
-          "F#6": "Fs6.mp3",
-          A6: "A6.mp3",
-          C7: "C7.mp3",
-          "D#7": "Ds7.mp3",
-          "F#7": "Fs7.mp3",
-          A7: "A7.mp3",
-          C8: "C8.mp3",
+          A0: "a (1).wav",
+          C1: "a (2).wav",
+          "D#1": "a (1).wav",
+          "F#1": "a (2).wav",
+          A1: "a (3).wav",
+          C2: "a (4).wav",
+          "D#2": "a (3).wav",
+          "F#2": "a (4).wav",
+          A2: "a (5).wav",
+          C3: "a (6).wav",
+          "D#3": "a (5).wav",
+          "F#3": "a (6).wav",
+          A3: "a (7).wav",
+          C4: "a (8).wav",
+          "D#4": "a (7).wav",
+          "F#4": "a (8).wav",
+          A4: "a (9).wav",
+          C5: "a (10).wav",
+          "D#5": "a (9).wav",
+          "F#5": "a (10).wav",
+          A5: "a (11).wav",
+          C6: "a (12).wav",
+          "D#6": "a (11).wav",
+          "F#6": "a (12).wav",
+          A6: "a (13).wav",
+          "D#7": "a ().wav",
+          "F#7": "a ().wav",
+          A7: "a ()",
+          C2: "a ().wav",
         },
    
         // Cela règle la durée de permanence des notes jouées
         release: 10,
    
         // Source locale des sons
-        // baseUrl: "./mp3/"
+        // baseUrl: "./wav/"
    
-        baseUrl: "https://tonejs.github.io/audio/salamander/",
+        //baseUrl: "https://tonejs.github.io/audio/salamander/",
+        baseUrl: './soundFX/'
+
       }).toDestination();
     }
 }
--- 09-Sound/main.js
+++ 09-Sound/main.js
@@ -3,46 +3,74 @@ import midiNote from "midi-note";
 import MidiPlayer from "./js/MidiPlayer";
 import Circle from "./js/Circle";
 
+let lastNote = null;
 const canvas = document.getElementsByTagName("canvas")[0];
+let isMouseDown = false;
 //fullscreen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
-
+const noteDelay = 200;
 const monPiano = new Piano();
 const allCircles = [];
 
-document.addEventListener("keydown", (event) => {
-  const monMidiPlayer = new MidiPlayer("debussy-clair-de-lune.mid");
-  monMidiPlayer.addEventListener("loaded", (midi) => {
-    console.log("Midi loaded", midi);
-    // reste du code fin de la parenthèse
-    midi.tracks.forEach((track) => {
-      console.log(track.notes);
-      track.notes.forEach((note) => {
-        console.log(note);
-        setTimeout(() => {
-          // monPiano.sampler.triggerAttack(midiNote(note.midi));
+function randomNote() {
+  const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+  const octaves = ['2', '3', '4', '5', '6'];
+  return notes[Math.floor(Math.random() * notes.length)] + octaves[Math.floor(Math.random() * octaves.length)];
+}
 
-          const cercle = new Circle(
-            -100,
-            Math.random() * window.innerHeight,
-            10,
-            "red",
-            ctx,
-            midiNote(note.midi),
-            monPiano
-          );
-          allCircles.push(cercle);
-        }, note.ticks * 2.5);
-      });
-    });
-  });
+canvas.addEventListener("mousedown", (event) => {
+  isMouseDown = true;
+  const note = randomNote();
+  lastNote = note;
+  const circle = new Circle(event.clientX, event.clientY, 10, "red", ctx, note, monPiano);
+  allCircles.push(circle);
+  monPiano.sampler.triggerAttack(note);
+});
+
+canvas.addEventListener("mouseup", () => {
+  isMouseDown = false;
+  if (lastNote) {
+    monPiano.sampler.triggerRelease(lastNote);
+  }
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (isMouseDown) {
+    const note = randomNote();
+    lastNote = note;
+    const circle = new Circle(event.clientX, event.clientY, 10, "red", ctx, note, monPiano);
+    allCircles.push(circle);
+    monPiano.sampler.triggerAttackRelease(note, "1n", "+0.5");
+  }
+});
+
+
+
+
+
+/* canvas.addEventListener("click", (event) => {
+  const note = midiNote(Math.floor(Math.random() * 126) + 1);
+  const cercle = new Circle(
+    event.clientX,
+    event.clientY,
+    10,
+    "red",
+    ctx,
+    note,
+    monPiano
+  );
+  allCircles.push(cercle);
+  monPiano.sampler.triggerAttack(note);
+}); */
+ 
+  
 
   // console.log("key", event.key);
   // const note = midiNote(Math.floor(Math.random() * 126) + 1);
   // monPiano.sampler.triggerAttack(note);
-});
+
 
 // const cercle = new Circle(100, 100, 50, "red", ctx);
 // allCircles.push(cercle);
@@ -52,7 +80,7 @@ function draw() {
   // console.log("draw");
 
   allCircles.forEach((cercle) => {
-    cercle.move();
+    // cercle.move();
     cercle.draw();
   });
 
@@ -63,3 +91,7 @@ function draw() {
 }
 
 draw();
+setVolume();
+function setVolume() {
+  monPiano.sampler.volume.value = -30;
+}
