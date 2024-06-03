90762d87f4c821db3d250f68247843056d705549
brikeld <129516277+brikeld@users.noreply.github.com>
2024-06-03 17:10:14 +0200

    V11
    Undo works for sound and everything. play works.

---
+++ SoundToyV11/js/DottedLine.js
@@ -0,0 +1,53 @@
+export default class DottedLine {
+  constructor(x1, y1, x2, y2, ctx, note = null, piano = null, dottedLineFillImage, blendMode = 'difference') {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.ctx = ctx;
+    this.note = note;
+    this.piano = piano;
+    this.circleRadius = 20;
+    this.circleSpacing = 20;
+    this.dottedLineFillImage = dottedLineFillImage;
+    this.blendMode = blendMode;
+    this.soundPlayed = false;
+    this.id = Math.random().toString(36).substring(2, 15); // Unique identifier for the dotted line
+  }
+
+  playNote() {
+    if (this.piano && this.note && !this.soundPlayed) {
+      console.log(`Playing note: ${this.note}`);
+      this.piano.sampler.triggerAttackRelease(this.note, "1n", "+0.1");
+      this.soundPlayed = true;
+    }
+  }
+
+  draw() {
+    const dx = this.x2 - this.x1;
+    const dy = this.y2 - this.y1;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const steps = Math.floor(distance / this.circleSpacing);
+    const xStep = dx / steps;
+    const yStep = dy / steps;
+
+    if (this.dottedLineFillImage.complete) {
+      const pattern = this.ctx.createPattern(this.dottedLineFillImage, "repeat");
+      this.ctx.fillStyle = pattern;
+    } else {
+      this.ctx.fillStyle = "black"; // Fallback color
+    }
+
+    this.ctx.globalCompositeOperation = this.blendMode;
+
+    for (let i = 0; i <= steps; i++) {
+      const x = this.x1 + i * xStep;
+      const y = this.y1 + i * yStep;
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, this.circleRadius, 0, 2 * Math.PI);
+      this.ctx.fill();
+    }
+
+    this.ctx.globalCompositeOperation = 'source-over';
+  }
+}
